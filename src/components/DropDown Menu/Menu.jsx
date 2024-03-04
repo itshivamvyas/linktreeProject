7b68ce751bbb8c51c5982a7c57470af4aebddc49
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-04 12:43:43 +0530

    Update Menu.jsx

--- src/components/DropDown Menu/Menu.jsx
+++ src/components/DropDown Menu/Menu.jsx
@@ -13,26 +13,26 @@ function Menu() {
     <div className="fixed right-4 z-50 top-[93px] shadow-2xl shadow-indigo-300">
       <div className="float-right bg-white rounded-lg">
         <ul>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-indigo-700 rounded-t-lg hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-t-lg hover:text-white active:translate-y-0.5">
             <IconUserScan /> Your Profile
           </li>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-indigo-700 hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
             <IconShirt /> Themes
           </li>
           <Link rel="stylesheet" to="/about-us">
-            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-indigo-700 hover:text-white active:translate-y-0.5">
+            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
               <IconRobot />
               About Us
             </li>
           </Link>
 
           <Link rel="stylesheet" to="/contact-us">
-            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-indigo-700 hover:text-white active:translate-y-0.5">
+            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
               <IconPhoneCall />
               Contact Us
             </li>
           </Link>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-indigo-700 rounded-b-lg hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-b-lg hover:text-white active:translate-y-0.5">
             <IconLogout /> Logout
           </li>
         </ul>
