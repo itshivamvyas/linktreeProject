bfc3030a3e682118567467e3e37145376841f0f8
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-04 19:33:51 +0530

    Update Modal

--- src/components/AddButtonModal/Modal.jsx
+++ src/components/AddButtonModal/Modal.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IconX } from "@tabler/icons-react";
 
-function Modal() {
-
-  const [title, setTitle] = useState("")
-  const [url, setUrl] = useState("")
+function Modal({ closeModal, addButton }) {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -13,49 +12,57 @@ function Modal() {
     };
   }, []);
 
-
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-stone-900 bg-opacity-50 backdrop-blur-[1px]">
-      <div className="border-[3px] border-white rounded-3xl min-w-[50%] min-h-[40%] p-[2px]">
-        <div className="bg-white rounded-3xl min-w-[50%] min-h-[40%] p-6 shadow-2xl flex flex-col justify-between gap-8">
-          <div className="text-right">
-            <button>
-              <IconX height={40} width={40} />
-            </button>
-          </div>
-
-          <div className="flex flex-col gap-8">
-            <div>
-              <h1 className="text-2xl p-2 select-none">
-                <label htmlFor="title">
-                  <h1 className="font-bold">Title</h1>
-                  <input
-                    type="text"
-                    id="title"
-                    placeholder="Enter Title"
-                    className="text-xl w-full p-4 outline-none"
-                  />
-                </label>
-              </h1>
-            </div>
-            <div>
-              <h1 className="text-2xl p-2 select-none">
-                <label htmlFor="url">
-                  <h1 className="font-bold">URL</h1>
-                  <input
-                    type="text"
-                    id="url"
-                    placeholder="Enter URL"
-                    className="text-xl w-full p-4 outline-none"
-                  />
-                </label>
-              </h1>
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-stone-900 bg-opacity-50 backdrop-blur-[1px] px-4"
+      onClick={closeModal}
+    >
+      <div
+        className="bg-white max-w-96 w-full rounded-3xl p-8 shadow-2xl flex flex-col justify-between gap-5 relative"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="absolute top-4 right-4 hover:bg-slate-200 text-stone-600 p-1 rounded-full cursor-pointer active:translate-y-0.5"
+          onClick={closeModal}
+        >
+          <IconX height={22} width={22} />
+        </button>
+        <div className="flex flex-col gap-4">
+          <div>
+            <div className="select-none">
+              <label htmlFor="title">
+                <p className="mb-2">Title</p>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="Enter Title"
+                  className="py-2 px-3 outline-none bg-gray-100 rounded-lg w-full focus:bg-gray-50 focus:outline-black"
+                  required
+                />
+              </label>
             </div>
           </div>
-
-          <div className="flex justify-center items-center text-xl gap-2 p-4 bg-black rounded-full text-white font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5">
-            <button className="select-none">Add Link</button>
+          <div>
+            <h1 className="select-none">
+              <label htmlFor="url">
+                <p className="mb-2">URL</p>
+                <input
+                  type="text"
+                  id="url"
+                  placeholder="Enter URL"
+                  className="py-2 px-3 outline-none bg-gray-100 rounded-lg w-full focus:bg-gray-50 focus:outline-black"
+                  required
+                />
+              </label>
+            </h1>
           </div>
+        </div>
+
+        <div
+          className="flex justify-center items-center gap-2 p-2 bg-black rounded-lg text-white font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5 mt-4"
+          onClick={addButton()}
+        >
+          <button className="select-none">Add Link</button>
         </div>
       </div>
     </div>
