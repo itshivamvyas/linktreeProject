b076ab9bcf57c292d886b718bb4407fc81877d1c
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-04 19:48:43 +0530

    Update Links And Menu

--- src/Pages/Links.jsx
+++ src/Pages/Links.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./Todo";
 import Preview from "../components/Preview Section/Preview";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-
-
+import Modal from "../components/AddButtonModal/Modal";
 
 function Links() {
+  const [linksData, setLinksData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const setData = () => {
+    setLinksData([
+      ...linksData,
+      {
+        title: `Title ${linksData.length + 1}`,
+        url: `https://example.com/${linksData.length + 1}`,
+      },
+    ]);
+  };
 
   const onAddClick = () => {
     window.scrollTo({
       top: 75,
-      behavior: "smooth"
+      behavior: "smooth",
     });
-  }
+
+    setShowModal(true);
+  };
+
+  const LinkComps = linksData.map((link) => (
+    <Todo key={link.title} title={link.title} url={link.url} />
+  ));
 
   return (
-    <>
-      <div className="pb-5">
+    <div>
+      <div className="min-h-[calc(100vh-105px)]">
         <div className="grid grid-cols-10">
           <div className="col-span-6">
             <section className="p-10 max-w-2xl mx-auto">
               <div className="bg-gray-200 pt-5 sticky top-[93px] mb-14 z-50 rounded-b-[32px]">
-                <button onClick={onAddClick} id="add" className="flex justify-center items-center gap-2 p-5 bg-indigo-800 rounded-full text-white font-bold w-full hover:bg-indigo-700 z-50">
+                <button
+                  onClick={onAddClick}
+                  id="add"
+                  className="flex justify-center items-center select-none gap-2 p-5 bg-black rounded-full text-white font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5"
+                >
                   <svg
                     width="22px"
                     height="22px"
@@ -38,21 +61,17 @@ function Links() {
                 </button>
               </div>
 
-              <Todo />
-              <Todo />
-              <Todo />
-              <Todo />
-              <Todo />
-              <Todo />
-              <Todo />
-              <Todo />
+              {LinkComps}
+
+              {showModal && <Modal closeModal={closeModal} addButton={setData} />}
+
             </section>
           </div>
 
           <Preview />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/DropDown Menu/Menu.jsx
+++ src/components/DropDown Menu/Menu.jsx
@@ -8,31 +8,31 @@ import {
 } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
-function Menu() {
+function Menu({onclickHandler}) {
   return (
-    <div className="fixed right-4 z-50 top-[93px] shadow-2xl shadow-indigo-300">
+    <div className="fixed right-4 z-50 top-[93px] shadow-2xl shadow-indigo-300 select-none">
       <div className="float-right bg-white rounded-lg">
         <ul>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-t-lg hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-t-lg hover:text-white active:translate-y-0.5" onClick={onclickHandler}>
             <IconUserScan /> Your Profile
           </li>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5" onClick={onclickHandler}>
             <IconShirt /> Themes
           </li>
           <Link rel="stylesheet" to="/about-us">
-            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
+            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5" onClick={onclickHandler}>
               <IconRobot />
               About Us
             </li>
           </Link>
 
           <Link rel="stylesheet" to="/contact-us">
-            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5">
+            <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black hover:text-white active:translate-y-0.5" onClick={onclickHandler}>
               <IconPhoneCall />
               Contact Us
             </li>
           </Link>
-          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-b-lg hover:text-white active:translate-y-0.5">
+          <li className="flex flex-row gap-3 p-3 cursor-pointer min-w-60 font-semibold hover:bg-black rounded-b-lg hover:text-white active:translate-y-0.5" onClick={onclickHandler}>
             <IconLogout /> Logout
           </li>
         </ul>
