4169b0565c205d4bcdcd217f2c72fa0597536c4a
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-15 12:56:12 +0530

    Add Preview Styling

--- src/components/Preview Section/Preview.jsx
+++ src/components/Preview Section/Preview.jsx
@@ -6,25 +6,28 @@ function Preview() {
   return (
     <div className="col-span-4 flex justify-center p-10">
       <div className="fixed select-none">
-        <div className="bg-white rounded-3xl p-4 border-8 border-black aspect-[9_/_19] h-[calc(100vh-150px)] overflow-y-auto no-scrollbar">
-          <div className="text-justify">
-            <div className="flex flex-col gap-2 justify-center items-center p-4">
+        <div className="bg-slate-200 rounded-3xl py-1 border-8 border-black aspect-[9_/_19] h-[calc(100vh-150px)]">
+          <div className="overflow-y-auto no-scrollbar h-full px-4">
+          <div className="text-justify ">
+            <div className="flex flex-col gap-1 justify-center items-center p-5">
             <img
                 className="rounded-full max-w-20 max-h-20"
                 src="./Shivam.jpg"
                 alt=""
               />
               <h1 className="font-bold">@Itshivamvyas</h1>
+              <h1>Software Developer</h1>
             </div>
 
             <div className="flex flex-col gap-3 cursor-pointer">
               {linksData.map((it) => (
-                <div key={it.title} className="bg-gray-300 p-3 text-center text-xs font-bold hover:scale-105 transition-transform rounded-xl">
+                <div key={it.title} className="bg-white p-3 text-center text-xs font-bold hover:scale-105 transition-transform rounded-lg shadow-inner shadow-blue-800/20">
                   <p>{it.title}</p>
                 </div>
               ))}
             </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
