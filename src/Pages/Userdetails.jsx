c87940329c9c16b72177cf14484279e9dcb9ebf2
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-16 14:49:55 +0530

    Update User Details

---
+++ src/Pages/Userdetails.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IconArrowLeft } from "@tabler/icons-react";
+
+function Userdetails() {
+  return (
+    <div className="bg-emerald-200 min-h-screen min-w-screen flex flex-col items-center justify-center">
+      <Link rel="stylesheet" to="/">
+        <IconArrowLeft className="absolute top-8 left-10 hover:brightness-125 active:translate-y-0.5 cursor-pointer rounded-full size-8 text-emerald-200 bg-emerald-900" />
+      </Link>
+      <div className="flex flex-col justify-center items-center w-full">
+        <div>
+          <img src="./user.png" alt="user" className="size-60" />
+        </div>
+        <form action="" className="flex flex-col gap-16 min-w-[700px] p-6">
+            <div className="flex flex-col gap-6">
+          <div>
+            <label htmlFor="name" className="flex flex-col gap-1">
+              <p className="ps-2">Your Name</p>
+              <input
+                type="text"
+                id="name"
+                placeholder="Enter Your Name"
+                className="p-3 rounded-xl bg-slate-100"
+              />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="username" className="flex flex-col gap-1">
+              <p className="ps-2">Your Username</p>
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter Your Username"
+                className="p-3 rounded-xl bg-slate-100"
+              />
+            </label>
+          </div>
+
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex justify-center items-center select-none gap-2 py-3 px-5 bg-emerald-900 rounded-xl text-emerald-200 font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Userdetails;
