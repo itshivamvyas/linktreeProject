b0bc739c73dd70415934a8b388c080154b9e228e
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-16 22:33:06 +0530

    Update Themes Page

--- src/Pages/Themes.jsx
+++ src/Pages/Themes.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { IconHome } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
 
 function Themes() {
   return (
-    <div>
-      I Have Many Themes For You Guys And I Show You Some Of Them Here
+    <div className='w-screen h-screen bg-white text-black flex flex-col gap-6 justify-center items-center'>
+      <p className='text-5xl font-semibold'>Themes Fetures Is Coming Soon....</p>
+      <p className='text-3xl font-semibold'>Back To Home</p>
+      <Link to="/" >
+      <div className='bg-slate-300 px-10 py-3 rounded-xl hover:bg-slate-400 active:translate-y-0.5'>
+      <IconHome className='size-16'/>
+        </div>
+        </Link>
     </div>
   )
 }
