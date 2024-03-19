bd1c3f96913f7c728cc701878537923052a7f911
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-19 10:58:18 +0530

    Update Animation

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
     <App/>
+    <Toaster position="bottom-center"/>
     </BrowserRouter>
   </React.StrictMode>
 );
