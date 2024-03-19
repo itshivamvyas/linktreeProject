d8a8bd8260e4e0f1c03c9ababe10a63f52164ec2
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-19 11:15:19 +0530

    Create Profile Paage

--- src/Pages/Yourprofile.jsx
+++ src/Pages/Yourprofile.jsx
@@ -10,48 +10,70 @@ function Yourprofile() {
       </Link>
       <div className="flex flex-col justify-center items-center w-full">
         <div className="flex flex-col justify-center items-center">
-          <img src="./user.png" alt="user" className="size-60 cursor-pointer rounded-full" />
+          <img
+            src="./user.png"
+            alt="user"
+            className="size-60 cursor-pointer rounded-full"
+          />
           <p className="cursor-pointer">Change Profile Picture</p>
         </div>
         <form action="" className="flex flex-col gap-16 min-w-[700px] p-6">
-            <div className="flex flex-col gap-6">
-          <div>
-            <label htmlFor="name" className="flex flex-col gap-1">
-              <p className="ps-2">Your Name</p>
-              <input
-                type="text"
-                id="name"
-                placeholder="Enter Your Name"
-                className="p-3 rounded-xl bg-slate-100 outline-none focus-within:ring-2 ring-black"
-              />
-            </label>
-          </div>
+          <div className="flex flex-col gap-6">
+            <div>
+              <label htmlFor="name" className="flex flex-col gap-1">
+                <p className="ps-2">Your Name</p>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Enter Your Name"
+                  className="p-3 rounded-xl bg-slate-100 outline-none focus-within:ring-2 ring-black"
+                />
+              </label>
+            </div>
 
-          <div>
-            <label htmlFor="username" className="flex flex-col gap-1">
-              <p className="ps-2">Your Username</p>
-              <input
-              disabled
-                type="text"
-                id="username"
-                placeholder="Enter Your Username"
-                className="p-3 rounded-xl bg-slate-100 outline-none focus-within:ring-2 ring-black"
-              />
-            </label>
-          </div>
+            <div>
+              <label htmlFor="email" className="flex flex-col gap-1">
+                <div className="flex items-center  gap-3">
+                  <p className="ps-2">Your Email</p>
+                  <p className="p-1 bg-slate-200 text-xs rounded-md cursor-pointer">
+                    Verify Your Email
+                  </p>
+                </div>
+                <input
+                  disabled
+                  type="email"
+                  id="email"
+                  placeholder="Enter Your Email"
+                  className="p-3 rounded-xl bg-slate-100 outline-none focus-within:ring-2 ring-black"
+                />
+              </label>
+            </div>
 
+            <div>
+              <label htmlFor="username" className="flex flex-col gap-1">
+                <p className="ps-2">Your Username</p>
+                <input
+                  disabled
+                  type="text"
+                  id="username"
+                  placeholder="Enter Your Username"
+                  className="p-3 rounded-xl bg-slate-100 outline-none focus-within:ring-2 ring-black"
+                />
+              </label>
+            </div>
           </div>
           <div>
             <button
               type="submit"
-              className="flex justify-center items-center select-none gap-2 py-3 px-5 bg-black rounded-xl text-white font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5">
+              className="flex justify-center items-center select-none gap-2 py-3 px-5 bg-black rounded-xl text-white font-bold w-full hover:bg-opacity-85 shadow-lg shadow-black/40 z-50 transition-transform active:translate-y-0.5"
+            >
               Save
             </button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Yourprofile
+export default Yourprofile;
