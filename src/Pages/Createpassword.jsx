2f54e37124b440d1c20100781ce2b8abaeac6240
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-20 18:13:19 +0530

    Improve Code Structure

--- src/Pages/Createpassword.jsx
+++ src/Pages/Createpassword.jsx
@@ -1,82 +1,88 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { IconEye, IconEyeOff, IconArrowLeft } from "@tabler/icons-react";
+import { IconArrowLeft } from "@tabler/icons-react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useAppContext } from "../App";
+import { toast } from "react-hot-toast";
+import { auth } from "../firebase";
+import { addOrUpdateUserDetail } from "../firebase/firestore";
 
 function Createpassword() {
-  const {signupEmailPassword, setCreatePasswordInput} = useAppContext()
-  const [maskPassword, setMaskPassword] = useState("password");
-  const togglePasswordVisibility = () => {
-    setMaskPassword(maskPassword === "password" ? "text" : "password");
-  };
-  const [passwordShowIcon, setPasswordShowIcon] = useState(false);
-  const IconReplace = passwordShowIcon ? IconEye : IconEyeOff;
+  const { signUpWithEmailAndPasswordEmailInput, usernameInput } =
+    useAppContext();
+  const [
+    signUpWithEmailAndPasswordPasswordInput,
+    setSignUpWithEmailAndPasswordPasswordInput,
+  ] = useState("");
 
-  const [inputValue, setInputValue] = useState("")
-
-  const OnclickPasswordButton = () => {
-    setPasswordShowIcon(!passwordShowIcon);
-    togglePasswordVisibility();
+  const onSubmit = async (email, password) => {
+    try {
+      const u = await createUserWithEmailAndPassword(auth, email, password);
+      await addOrUpdateUserDetail(u.user.uid, {
+        email: u.user.email,
+        username: usernameInput,
+      });
+    } catch (error) {
+      toast.error("Something went wrong try again after sometime");
+      console.log(error);
+    }
   };
+
   return (
     <div className="bg-emerald-200 min-h-screen w-screen flex flex-col items-center justify-center">
-    <Link rel="stylesheet" to="/signup">
-      <IconArrowLeft className="absolute top-8 left-10 hover:brightness-125 active:translate-y-0.5 cursor-pointer rounded-full size-8 text-emerald-200 bg-emerald-900" />
-    </Link>
-    <div className="flex flex-col justify-center items-center gap-10">
-      <div className="flex flex-col justify-center items-center gap-3">
-        <div className="text-5xl font-extrabold text-emerald-900">
-          Create a Strong Password
+      <Link rel="stylesheet" to="/signup">
+        <IconArrowLeft className="absolute top-8 left-10 hover:brightness-125 active:translate-y-0.5 cursor-pointer rounded-full size-8 text-emerald-200 bg-emerald-900" />
+      </Link>
+      <div className="flex flex-col justify-center items-center gap-10">
+        <div className="flex flex-col justify-center items-center gap-3">
+          <div className="text-5xl font-extrabold text-emerald-900">
+            Create a Strong Password
+          </div>
+          <div className="text-xl text-emerald-900">
+            Create a strong password with at least 8 characters.
+          </div>
         </div>
-        <div className="text-xl text-emerald-900">
-        Create a strong password with at least 8 characters.
+
+        <div className="flex flex-col justify-center items-center gap-3 w-full">
+          <div className="flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black">
+            <input
+              type="password"
+              onChange={(e) => {
+                setSignUpWithEmailAndPasswordPasswordInput(e.target.value);
+              }}
+              value={signUpWithEmailAndPasswordPasswordInput}
+              placeholder="Create Password"
+              className="p-3 w-full rounded-lg bg-white outline-none"
+            />
+          </div>
+
+          <div className="flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black">
+            <input
+              type="text"
+              defaultValue={signUpWithEmailAndPasswordPasswordInput}
+              disabled
+              placeholder="Confirm Password"
+              className="p-3 w-full rounded-lg bg-white outline-none"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="flex flex-col justify-center items-center gap-3 w-full">
-      <div className="flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black">
-        <input
-          type={maskPassword}
-          onChange={(e)=>{
-            setInputValue(e.target.value)
-            setCreatePasswordInput(e.target.value)
-          }}
-          placeholder="Create Password"
-          className="p-3 w-full rounded-lg bg-white outline-none"
-        />
-        {/* <IconReplace
-          onClick={OnclickPasswordButton}
-          className="cursor-pointer mr-3"
-        /> */}
-      </div>
-
-      <div className="flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black">
-        <input
-          type="text"
-          defaultValue={inputValue}
-          disabled
-          placeholder="Confirm Password"
-          className="p-3 w-full rounded-lg bg-white outline-none"
-        />
-
-    
-        {/* <IconReplace
-          onClick={OnclickPasswordButton}
-          className="cursor-pointer mr-3"
-        /> */}
-      </div>
-      </div>
-
-      <div className="flex flex-col justify-center items-center gap-3 w-full">
-        <div className="w-full">
-        {/* <Link to="/userdetails"> */}
-          <button onClick={signupEmailPassword} className="flex justify-center bg-emerald-900 items-center select-none p-3 rounded-full text-white font-bold w-full hover:brightness-125 z-50 transition-transform active:translate-y-0.5">
-            Create Password
-          </button>
-        {/* </Link> */}
+        <div className="flex flex-col justify-center items-center gap-3 w-full">
+          <div className="w-full">
+            <button
+              onClick={() =>
+                onSubmit(
+                  signUpWithEmailAndPasswordEmailInput,
+                  signUpWithEmailAndPasswordPasswordInput
+                )
+              }
+              className="flex justify-center bg-emerald-900 items-center select-none p-3 rounded-full text-white font-bold w-full hover:brightness-125 z-50 transition-transform active:translate-y-0.5"
+            >
+              Create Password
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,12 +1,14 @@
 import {
   addDoc,
   collection,
-  doc,
   getFirestore,
   updateDoc,
   query,
   where,
   getDocs,
+  setDoc,
+  doc,
+  getDoc,
 } from "firebase/firestore";
 
 const db = getFirestore();
@@ -14,24 +16,20 @@ const db = getFirestore();
 export const usersDetailRef = collection(db, "users-detail");
 
 // name, picture, email, uid, username
-export async function addUserDetail(data) {
-  await addDoc(usersDetailRef, data);
+export async function addOrUpdateUserDetail(uid, data) {
+  try {
+    await setDoc(doc(usersDetailRef, uid), data, { merge: true });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// export async function getUsersDetail(uid, data){
-
-// }
-
-export async function updateUserDetail(uid, data) {
-  const q = query(usersDetailRef, where("uid", "==", uid));
-  const docs = await getDocs(q);
-
-  docs.forEach(async (d) => {
-    try {
-        
-        await updateDoc(d.ref, {username: "loolol"});
-    } catch (error) {
-        console.log(error)
-    }
-  });
+export async function getUserDetailData(uid) {
+  try {
+    const dataDoc = await getDoc(doc(usersDetailRef, uid));
+    if (data.exists) return dataDoc.data();
+    else return null;
+  } catch (error) {
+    console.log(error);
+  }
 }
