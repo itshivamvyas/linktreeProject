241fe475b5912d0086e3391555a989febba5a505
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-03-22 16:43:34 +0530

    Error Handling And State Updation And Email Checker

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -9,17 +9,21 @@ import {
 import { useAppContext } from "../App";
 import { toast } from "react-hot-toast";
 import ButtonLoading from "../components/ButtonLoading/ButtonLoading";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { addOrUpdateUserDetail } from "../firebase/firestore";
+import { auth, provider } from "../firebase";
 
 function LogIn() {
-  const [signInEmailInput, setSignInEmailInput] = useState("");
-  const { signInPasswordInput, setSignInPasswordInput, usernameInput } =
+  const [logInEmailInput, setLogInEmailInput] = useState("");
+  const [logInPasswordInput, setLogInPasswordInput] = useState("")
+  const { usernameInput } =
     useAppContext();
   const [maskPassword, setMaskPassword] = useState("password");
 
 const [isLoading, setIsLoading] = useState(false)
 const [emailInputError, setEmailInputError] = useState(false)
 const [passwordInputError, setPasswordInputError] = useState(false)
+const [isGoogleButtonLoading, setIsGoogleButtonLoading] = useState(false);
 
   const togglePasswordVisibility = () => {
     setMaskPassword(maskPassword === "password" ? "text" : "password");
@@ -31,9 +35,27 @@ const [passwordInputError, setPasswordInputError] = useState(false)
     togglePasswordVisibility();
   };
 
+  const loginWithGoogle = async (usernameInput) => {
+    setIsGoogleButtonLoading(true);
+    try {
+      const u = await signInWithPopup(auth, provider);
+      await addOrUpdateUserDetail(u.user.uid, {
+        name: u.user.displayName,
+        email: u.user.email,
+        photo: u.user.photoURL,
+        username: usernameInput
+      });
+      setIsGoogleButtonLoading(false);
+      toast.success("Login Successful");
+    } catch (error) {
+      setIsGoogleButtonLoading(false);
+      toast.error(error.code);
+    }
+  };
+
   const emailCheck = () => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(signInEmailInput);
+    return regex.test(logInEmailInput);
   }
 
   async function logInWithEmailPassword(email, password) {
@@ -42,7 +64,7 @@ const [passwordInputError, setPasswordInputError] = useState(false)
       return;
     }
 
-    if(signInPasswordInput.length < 6){
+    if(logInPasswordInput.length < 6){
       setPasswordInputError(true)
       return;
     }
@@ -55,8 +77,9 @@ const [passwordInputError, setPasswordInputError] = useState(false)
       toast.success("Login Successful");
       setIsLoading(false)
     } catch (error) {
-      console.log(error);
-      toast.error("Invalid Email And Password");
+      setIsLoading(false)
+      console.log(error.code);
+      toast.error(error.code);
     }
   }
 
@@ -78,33 +101,37 @@ const [passwordInputError, setPasswordInputError] = useState(false)
         <div className="flex flex-col justify-center items-center gap-3 w-full">
           <input
             type="email"
-            value={signInEmailInput}
+            value={logInEmailInput}
             onChange={(e) => {
               setEmailInputError(false)
-              setSignInEmailInput(e.target.value)}}
+              setLogInEmailInput(e.target.value)}}
             placeholder="Email"
             className={`${emailInputError ? "text-red-600 ring-2 ring-red-600":""} p-3 w-full rounded-lg bg-white outline-none focus-within:ring-2 ring-black`}
           />
-          <div className="flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black">
+          <div className={`${passwordInputError ? "ring-2 ring-red-600 text-red-600":""} flex items-center bg-white rounded-lg w-full focus-within:ring-2 ring-black`}>
             <input
               type={maskPassword}
-              value={signInPasswordInput}
-              onChange={(e) => {setSignInPasswordInput(e.target.value)}}
+              value={logInPasswordInput}
+              onChange={(e) =>{
+                setPasswordInputError(false)
+                setLogInPasswordInput(e.target.value)}}
               placeholder="Password"
-              className={`${passwordInputError ? "ring-2 ring-red-600 text-red-600":""} p-3 w-full rounded-lg bg-white outline-none`}
+              className="p-3 w-full rounded-lg bg-white outline-none"
             />
             <IconReplace
               onClick={OnclickPasswordButton}
               className="cursor-pointer mr-3"
             />
           </div>
+          <div className="w-full float-left">
           {passwordInputError && <p className="font-semibold text-red-600">Password must be at least 6 characters long</p>}
+        </div>
         </div>
 
         <div className="flex flex-col justify-center items-center gap-3 w-full">
           <button
             onClick={() =>
-              logInWithEmailPassword(signInEmailInput, signInPasswordInput)
+              logInWithEmailPassword(logInEmailInput, logInPasswordInput)
             }
             className="flex justify-center bg-emerald-900 items-center select-none p-3 rounded-full text-white font-bold w-full hover:brightness-125 z-50 transition-transform active:translate-y-0.5"
           >
@@ -115,8 +142,14 @@ const [passwordInputError, setPasswordInputError] = useState(false)
             onClick={() => loginWithGoogle(usernameInput)}
             className="flex justify-center gap-3 bg-emerald-900 items-center select-none p-3 rounded-full text-white font-bold w-full hover:brightness-125 z-50 transition-transform active:translate-y-0.5"
           >
-            <IconBrandGoogleFilled />
-            Login With Google
+            {isGoogleButtonLoading ? (
+              <ButtonLoading />
+            ) : (
+              <>
+                <IconBrandGoogleFilled />
+                <span>Log In With Google</span>
+              </>
+            )}
           </button>
         </div>
 
@@ -134,7 +167,7 @@ const [passwordInputError, setPasswordInputError] = useState(false)
 
           <div>
             <div>
-              You can also log in with a{" "}
+              You can also Log In with a{" "}
               <span className="text-blue-900 hover:underline cursor-pointer font-semibold">
                 <Link rel="stylesheet" to="/loginbyphone">
                   Phone number
