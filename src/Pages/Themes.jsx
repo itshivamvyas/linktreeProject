2dbb566ac88f05673791a782d295d91f133dfedf
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-04-05 13:27:53 +0530

    Change Size Of Text

--- src/Pages/Themes.jsx
+++ src/Pages/Themes.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 function Themes() {
   return (
-    <div className='w-screen h-screen bg-white text-black flex flex-col gap-6 justify-center items-center'>
-      <p className='text-5xl font-semibold'>Themes Fetures Is Coming Soon....</p>
-      <p className='text-3xl font-semibold'>Back To Home</p>
+    <div className='w-screen h-screen bg-white text-black flex flex-col gap-4 justify-center items-center'>
+      <p className='text-2xl font-semibold'>Themes Fetures Is Coming Soon....</p>
+      <p className='text-xl font-semibold'>Back To Home</p>
       <Link to="/" >
       <div className='bg-slate-300 px-10 py-3 rounded-xl hover:bg-slate-400 active:translate-y-0.5'>
-      <IconHome className='size-16'/>
+      <IconHome className='size-8'/>
         </div>
         </Link>
     </div>
