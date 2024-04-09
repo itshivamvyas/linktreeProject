18f2505d387c4d6a4255fd34a5d6c322201b5a0e
Shivam Kumar <shivamkumarvyas19@gmail.com>
2024-04-09 08:08:17 +0530

    Improve Fetures

--- src/Pages/Userdetails.jsx
+++ src/Pages/Userdetails.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { IconArrowLeft } from "@tabler/icons-react";
 import { useAppContext } from "../App";
 
 function Userdetails() {
